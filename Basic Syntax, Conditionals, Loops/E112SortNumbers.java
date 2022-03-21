3ed613f480f245c851bfd60842f14119e26fcb07
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-03-21 21:56:53 +0200

    Update and rename E12SortNumbers.java to E112SortNumbers.java

--- Basic Syntax, Conditionals, Loops/E112SortNumbers.java
+++ Basic Syntax, Conditionals, Loops/E112SortNumbers.java
@@ -1,6 +1,6 @@
 package Exercise1;
 import java.util.Scanner;
-public class E12SortNumbers {
+public class E112SortNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
