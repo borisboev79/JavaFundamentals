a9853e59d834eb9ba2823ddac0d5a772b8ec5d23
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-03-21 21:29:08 +0200

    Update and rename E05Login.java to E105Login.java

--- E105Login.java
+++ E105Login.java
@@ -2,7 +2,7 @@ package Exercise1;
 
 import java.util.Scanner;
 
-public class E05Login {
+public class E105Login {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String username = scanner.nextLine();
