9a6dfbb32ae9d56fac6839b56d688a988ccdf2c9
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-03-27 18:22:15 +0300

    Add files via upload

--- FinalExams/E1012MirrorWords.java
+++ FinalExams/E1012MirrorWords.java
@@ -10,44 +10,32 @@ public class E1012MirrorWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        Pattern pattern = Pattern.compile("([@#])(?<text>[A-Za-z]{3,}\\1\\1[A-Za-z]{3,})\\1");
+        Pattern pattern = Pattern.compile("([@#])(?<word1>[A-Za-z]{3,})\\1\\1(?<word2>[A-Za-z]{3,})\\1");
         Matcher matcher = pattern.matcher(input);
-        List<String> wordMatches = new LinkedList<>();
+        List<String> wordMatches1 = new LinkedList<>();
+        List<String> wordMatches2 = new LinkedList<>();
         List<String> results = new LinkedList<>();
-        StringBuilder mirror = new StringBuilder();
+
         while (matcher.find()) {
-            wordMatches.add(matcher.group("text"));
+            wordMatches1.add(matcher.group("word1"));
+            wordMatches2.add(matcher.group("word2"));
         }
-        if (!wordMatches.isEmpty()) {
-            System.out.printf("%d word pairs found!%n", wordMatches.size());
+        if (!wordMatches1.isEmpty()) {
+            System.out.printf("%d word pairs found!%n", wordMatches1.size());
         } else {
             System.out.println("No word pairs found!");
         }
 
-        for (String wordMatch : wordMatches) {
-            mirror.setLength(0);
-
-            String word = wordMatch.replaceAll("[@#]", "");
-            if (word.length() % 2 == 0) {
-
-                for (int j = 0; j < word.length() / 2; j++) {
-
-                    if (word.charAt(j) == word.charAt(word.length() - 1 - j)) {
-                        mirror.append(word.charAt(j));
-                    } else {
-                        mirror.setLength(0);
-                        break;
-                    }
-                }
-                String mirrorStraight = mirror.toString();
-                mirror.reverse();
-
-                if (mirror.length() > 0) {
-                    results.add(mirrorStraight + " <=> " + mirror);
-                }
+        for (int i = 0; i < wordMatches1.size(); i++) {
+            String word1 = wordMatches1.get(i);
+            StringBuilder temp = new StringBuilder(wordMatches2.get(i));
+            String word2 = temp.reverse().toString();
+            if (word1.equals(word2)) {
+                results.add(word1 + " <=> " + temp.reverse());
             }
+
         }
-        if (mirror.length() > 0) {
+        if (results.size() > 0) {
             System.out.println("The mirror words are:");
             System.out.println(String.join(", ", results));
         } else {
