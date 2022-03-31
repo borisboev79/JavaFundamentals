710ca64482548422b32362aec2822a5880584ba1
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-03-31 10:00:21 +0300

    Add files via upload

---
+++ WebApplicationDemo/Phonebook/src/main/java/phonebook/controller/HomeController.java
@@ -0,0 +1,51 @@
+package phonebook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.servlet.ModelAndView;
+import phonebook.entity.Contact;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+@Controller
+public class HomeController {
+    Map<String, Contact> contacts;
+
+    public HomeController(){
+        this.contacts = new TreeMap<>();
+    }
+
+    @GetMapping("/")
+    public ModelAndView home(ModelAndView modelAndView){
+        modelAndView.setViewName("index");
+        modelAndView.addObject("contacts", this.contacts);
+        return modelAndView;
+    }
+    @PostMapping("/")
+    public String add(Contact contact){
+        this.contacts.put(contact.getName(), contact);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit{name}")
+    public ModelAndView editForm(@PathVariable String name, ModelAndView modelAndView){
+        modelAndView.setViewName("edit");
+        modelAndView.addObject("contact", contacts.get(name));
+
+        return modelAndView;
+    }
+
+    @PutMapping("/edit{name}")
+    public String edit(@PathVariable String name, Contact contact){
+        Contact contact1 = contacts.remove(name);
+        contact1.setName(contact.getName());
+        contact1.setNumber(contact.getNumber());
+        contacts.put(contact.getName(), contact1);
+        return "redirect:/";
+    }
+}
+
