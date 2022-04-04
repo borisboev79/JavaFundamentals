7978d0d7879ef6b3b74221989a2697150798cf92
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-04-04 20:55:06 +0300

    Add files via upload

---
+++ Lists/E503HouseParty.java
@@ -0,0 +1,32 @@
+package Excercise5_Lists;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class E503HouseParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> guests = new ArrayList<>();
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+            String line = scanner.nextLine();
+            String[] data = line.split("\\s+");
+            String name = data[0];
+            if (!line.contains("not")){
+                if(guests.contains(name)){
+                    System.out.printf("%s is already in the list!%n", name);
+                }else{
+                    guests.add(name);
+                }
+            }else{
+                if(guests.contains(name)){
+                    guests.remove(name);
+                }else{
+                    System.out.printf("%s is not in the list!%n", name);
+                }
+            }
+
+        }
+
+guests.forEach(System.out::println);
+    }
+}
