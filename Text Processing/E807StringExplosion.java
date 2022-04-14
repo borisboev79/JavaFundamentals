2d07b12829c58179886dd93261f5a75561ec6e2b
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-04-14 22:18:27 +0300

    Add files via upload

---
+++ Text Processing/E807StringExplosion.java
@@ -0,0 +1,21 @@
+package Excercise8_TextProcessing;
+import java.util.Scanner;
+public class E807StringExplosion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder(scanner.nextLine());
+        int strength = 0;
+        for (int i = 0; i < sb.length() ; i++) {
+         if(strength > 0 && sb.charAt(i) != '>'){
+             sb.deleteCharAt(i);
+             strength--;
+             i--;
+         }else if(sb.charAt(i) == '>'){
+             strength += Integer.parseInt(String.valueOf(sb.charAt(i + 1)));
+
+         }
+
+        }
+        System.out.println(sb);
+    }
+}
